f7b411c6a36efd971a29227a0a497eec1c8941f4
Joel Robert Justiawan <joelwindows7@gmail.com>
2019-02-12 18:40:14 +0700

    Update hello_world_i2c_lcd_special.ino
    must include link

--- hello_world_i2c_lcd_special/hello_world_i2c_lcd_special.ino
+++ hello_world_i2c_lcd_special/hello_world_i2c_lcd_special.ino
@@ -12,6 +12,8 @@
 
     https://synapse.kyoto/lib/KanaLiquidCrystal/page001.html
     https://synapse.kyoto/lib/MGLCD/CharacterCode/page001.html
+
+    https://github.com/fdebrabander/Arduino-LiquidCrystal-I2C-library
  */
 
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
